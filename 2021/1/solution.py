cae75065b7117a14aa70607697bfd60ea906be19
Reed Swiernik <reed@plum.wine>
2021-12-02 14:34:25 -0800

    Solution 1: Part 2

--- 2021/1/solution.py
+++ 2021/1/solution.py
@@ -2,7 +2,7 @@ import sys
 from typing import List
 
 def find_increased(input: List) -> int:
-    years_increased = 0
+    depth_increased = 0
 
     current_count = None
     previous_count = None
@@ -10,10 +10,30 @@ def find_increased(input: List) -> int:
     for current_count in input:
         if previous_count and \
             current_count > previous_count:
-                years_increased += 1
+                depth_increased += 1
         previous_count = current_count
 
-    return years_increased
+    return depth_increased
+
+def find_increased_sliding_window(input: List) -> int:
+    depth_increased = 0
+    window = []
+    for count in input:
+        window.append(count)
+        if len(window) < 4:
+            continue
+        elif len(window) == 5:
+            window.pop(0)
+
+        a_sum = sum(window[:-1])  # First Window (AAA)
+        b_sum = sum(window[1:])  # Second Window (BBB)
+
+
+        if b_sum > a_sum:
+            depth_increased += 1
+
+    return depth_increased
+
 
 if __name__ == "__main__":
     in_file = sys.argv[1]
@@ -29,3 +49,5 @@ if __name__ == "__main__":
     num_increased = find_increased(input_array)
     print(num_increased)
 
+    num_increased = find_increased_sliding_window(input_array)
+    print(num_increased)
