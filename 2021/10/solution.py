925bd580b877faf1e8bb37f81c26b83fa2f23f39
Reed Swiernik <reed@plum.wine>
2021-12-12 18:12:39 -0800

    Solution 10: Part 2

--- 2021/10/solution.py
+++ 2021/10/solution.py
@@ -1,13 +1,20 @@
 import sys
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 
 
-score = {
+char_score = {
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137,
+}
+
+symbol_score = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
 }
 
 symbols = {
@@ -17,8 +24,9 @@ symbols = {
     '<': '>',
 }
 
-def check_syntax(line: str) -> str:
+def check_syntax(line: str) -> (Optional[str], List[str]):
     stack = []
+    # Part 1
     for symbol in line:
         if symbol in symbols.keys():
             stack.append(symbol)
@@ -30,13 +38,34 @@ def check_syntax(line: str) -> str:
                     expected,
                     symbol
                 ))
-                return symbol
+                return symbol, []
+
+    # Part 2
+    if len(stack) != 0:
+        # Incomplete!
+        ending = []
+        for item in stack[::-1]:
+            ending.append(symbols[item])
+    return None, ending
+
+
 
 def score_chars(chars: List[str]) -> int:
     total = 0
     for char in chars:
-        total += score[char]
+        total += char_score[char]
     return total
+
+
+def score_endings(endings: List[str]) -> int:
+    scores = []
+    for ending in endings:
+        total = 0
+        for char in ending:
+            total = total * 5
+            total += symbol_score[char]
+        scores.append(total)
+    return scores
 
 
 if __name__ == "__main__":
@@ -49,10 +78,19 @@ if __name__ == "__main__":
                 input.append(line.strip())
     # Part 1
     illegal_chars = []
+    endings = []
     for line in input:
-        c = check_syntax(line)
+        c, ending = check_syntax(line)
         if c:
             illegal_chars.append(c)
+        elif ending:
+            endings.append(ending)
 
+    # part 1
     print(illegal_chars)
-    print(score_chars(illegal_chars))
+    print('illegal characters score: {}'.format(score_chars(illegal_chars)))
+
+    # part 2
+    # print(endings)
+    ending_scores = score_endings(endings)
+    print('Winner: {}'.format(sorted(ending_scores)[int(len(ending_scores)/2)]))
